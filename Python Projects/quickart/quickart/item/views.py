601416d69994d995da5e305d9774eb3b6f6e50f9
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-01-25 11:13:16 +0530

    Updated details in views.py

--- Python Projects/quickart/quickart/item/views.py
+++ Python Projects/quickart/quickart/item/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
 
 # Create your views here.
+def detail(request, pk):
+    #pk = primary key
+    item = get_object_or_404(Item, pk=pk) #from model and from url
+    return render(request, 'item/detail.html', {
+        'item': item
+    })
