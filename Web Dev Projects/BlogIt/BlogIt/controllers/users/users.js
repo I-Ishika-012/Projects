5d1e2420e4b414a285f58690d891bbceb0965986
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-03-30 11:03:37 +0530

    Update users.js

--- Web Dev Projects/BlogIt/BlogIt/controllers/users/users.js
+++ Web Dev Projects/BlogIt/BlogIt/controllers/users/users.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const User = require("../../models/user/user");
+const User = require("../../models/user/User");
 const appErr = require("../../utils/appErr");
 
 
