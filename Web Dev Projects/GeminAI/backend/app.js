f77cfeff93fe6e74b2e768b0de4eba10c263906b
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-12 20:55:47 +0530

    Update app.js

--- Web Dev Projects/GeminAI/backend/app.js
+++ Web Dev Projects/GeminAI/backend/app.js
@@ -9,24 +9,27 @@ const app = express();
 //!middleware
 app.use(express.json());
 
+//!generate content route
+app.post("/generate", async (req, res) => {
+    const { prompt } = req.body;
+    try {
+    // For text-only input, use the gemini-pro model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text(); 
+   res.send(text);
+    }
+    catch (error) {
+        res.status(500).send({ message: "Internal server error| Failed to generate content" });
+    }
+   
+});
+
 // Access your API key as an environment variable (see "Set up your API key" above) const genAI = new GoogleGenerativeAI (process.env.API_KEY);
 const genAI=new GoogleGenerativeAI(process.env.API_KEY);
 
-// Define a function to run the model
-async function run() {
 
-// For text-only input, use the gemini-pro model
-const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-
-// Define a prompt
-const prompt = "Write a story about a magic backpack."
-const result = await model.generateContent(prompt);
-const response = await result.response;
-const text = response.text(); 
-console.log(text);
-}
-
-// Run the model
 //!start the server
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
