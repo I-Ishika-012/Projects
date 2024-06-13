522e16e20d83e2abfcebe358a41f9fb6b420f4e0
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-14 00:53:59 +0530

    Update main.js

--- Web Dev Projects/BakeIt/assets/js/main.js
+++ Web Dev Projects/BakeIt/assets/js/main.js
@@ -22,12 +22,18 @@ if(navClose){
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav-link')
 
-const linkActive = () =>{
+const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }   
 
-navLink.forEach(n => n.addEventListener('click', linkActive))
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
 
 /*=============== ADD BLUR HEADER ===============*/
 
