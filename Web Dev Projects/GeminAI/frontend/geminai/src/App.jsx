b49ce8670046466c90e08e56cf6177cbceeaa8c7
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-13 11:41:40 +0530

    Update App.jsx

--- Web Dev Projects/GeminAI/frontend/geminai/src/App.jsx
+++ Web Dev Projects/GeminAI/frontend/geminai/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 
+import axios from 'axios'
+import { useMutation } from 'react-query'
+import logo from './public/geminai-logo.png'
 // import './App.css'
+
+//!Function that must return a promise (useMutation)
+const makeRequestAPI = async () => {
+  const res = await axios.post("https://localhost:5173s/generate" )
+  return res.data;
+};
 
 function App() {
 
-
   return (
-    <>
-     
-    </>
+   <div className="App">
+    <header className="App-header">
+      <img src={logo}/>
+      <h1>AI Content Generator</h1>
+      </header>
+      <p></p>
+   </div>>
   )
 }
 
