89675fb340d2b81b0469d279e157dfce77501340
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-23 00:22:36 +0530

    Update main.js

--- Web Dev Projects/BakeIt/assets/js/main.js
+++ Web Dev Projects/BakeIt/assets/js/main.js
@@ -50,7 +50,7 @@ const scrollActive = () =>{
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 50,
+            sectionTop = current.offsetTop - 58,
             sectionId = current.getAttribute('id'),
 
             sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']')
