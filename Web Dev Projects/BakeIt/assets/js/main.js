40509f411bbc9574781725b23f766d193aedf70d
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-24 01:33:25 +0530

    Update main.js

--- Web Dev Projects/BakeIt/assets/js/main.js
+++ Web Dev Projects/BakeIt/assets/js/main.js
@@ -67,4 +67,25 @@ window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    opacity: 1,
+    scale : 1.1,
+    // reset: true
+})
 
+sr.reveal(`.home-data, .about__img, .about__data, .visit__data`)
+sr.reveal (`.home-img, .footer__img-1, .footer__img-2`, {rotate: {z: -15}})
+sr.reveal (`.home-bread`, {rotate: {z: 15}})
+sr.reveal (`.home-footer`, {scale: 1, origin: 'bottom'})
+
+sr.reveal (`.new__card:nth-child(1) img`, {rotate: {z: -30}, distance: 0})
+sr.reveal (`.new__card:nth-child(2) img`, {rotate: {z: -15}, distance: 0, delay: 600})
+sr.reveal (`.new__card:nth-child(3) img`, {rotate: {z: -30}, distance: 0, delay: 900})
+
+sr.reveal (`.favorite__card img`, {interval: 100, rotate: {z: 15}, distance: 0})
+
+sr.reveal (`.footer__container`, {scale: 1})
